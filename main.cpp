f20e85ead4c05cf16778d5a9898ec5d700926b4f
alex_malder <vnmntn@gmail.com>
2021-12-28 18:53:38 +0300

    new-delete-first-level-objects

--- main.cpp
+++ main.cpp
@@ -16,15 +16,17 @@ int main(int argc, char **argv)
     });
 
     srv.Post("/sign-in", [&rep](const Request &req, Response &res) {
-        Account account(rep);
-        Response_t r = account.sign_in(req.body);
+        Account *acc = new Account(rep);
+        Response_t r = acc->sign_in(req.body);
+        delete acc;
         res.status = r.status;
         res.set_content(r.body, "application/json");
     });
 
     srv.Post("/sign-up", [&rep](const Request &req, Response &res) {
-        Account account(rep);
-        Response_t r = account.sign_up(req.body);
+        Account *acc = new Account(rep);
+        Response_t r = acc->sign_up(req.body);
+        delete acc;
         res.status = r.status;
         res.set_content(r.body, "application/json");
     });
@@ -35,17 +37,18 @@ int main(int argc, char **argv)
                     req.get_param_value("start_date"),
                     req.get_param_value("end_date"));
 
-        Crypto crypto(rep);
-        Response_t r = crypto.get(token, query);
+        Crypto *crypto = new Crypto(rep);
+        Response_t r = crypto->get(token, query);
+        delete crypto;
         res.status = r.status;
         res.set_content(r.body, "application/json");
     });
 
     srv.Post("/crypto", [&rep](const Request &req, Response &res) {
         string token = req.get_header_value("authorization");
-        Crypto crypto(rep);
-        Response_t r = crypto.post(token, req.body);
-        res.status = r.status;
+        Crypto *crypto = new Crypto(rep);
+        Response_t r = crypto->post(token, req.body);
+        delete crypto;
         res.set_content(r.body, "application/json");
     });
     cout << "server listening port 5000" << endl;
