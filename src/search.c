1019b0dbb3658456e25b998680592a4e9e1802e9
alex_malder <vnmntn@gmail.com>
2022-01-11 02:36:22 +0300

    array.c

--- src/search.c
+++ src/search.c
@@ -11,46 +11,6 @@ double cosine_similarity(double *a, double *b, uint64_t end)
     return dot / (sqrt(denom_a) * sqrt(denom_b));
 }
 
-typedef struct {
-    double *array;
-    size_t used;
-    size_t size;
-} Array;
-
-void initArray(Array *a, size_t initialSize)
-{
-    a->array = malloc(initialSize * sizeof(double));
-    a->used = 0;
-    a->size = initialSize;
-}
-
-void insertArray(Array *a, int element)
-{
-    // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
-    // Therefore a->used can go up to a->size
-    if (a->used == a->size) {
-        a->size *= 2;
-        a->array = realloc(a->array, a->size * sizeof(int));
-    }
-    a->array[a->used++] = element;
-}
-
-void freeArray(Array *a)
-{
-    free(a->array);
-    a->array = NULL;
-    a->used = a->size = 0;
-}
-
-void debugArray(Array *a, uint64_t length)
-{
-    printf("[");
-    for (uint64_t i = 0; i < length; i++) {
-        printf(" %lf ", a->array[i]);
-    }
-    printf("]");
-}
-
 int selector_test()
 {
     // extract all
@@ -68,47 +28,40 @@ int selector_test()
     uint64_t interval = max_unix - min_unix; // get interval
     uint64_t ssize = interval / day_unix; // size of array
     // initialize arrays
-    Array a_target;
-    initArray(&a_target, tuple_count);
+    struct array_t b;
+    init_array(&b, tuple_count);
     // extract by range, extract by currency type
     //std::vector<double> target;
     for (uint64_t i = 0; i < tuple_count; i++) {
         if (fulldata[i].unix_val >= min_unix &&
             fulldata[i].unix_val < max_unix) {
             //target.push_back(fulldata[i].close);
-            insertArray(&a_target, fulldata[i].close);
+            insert_array(&b, fulldata[i].close);
         }
     }
-    uint64_t search_count = 0;
-    uint64_t insertion_count = 0;
     //uint64_t resolution = 256;
     double thresh = 0.9955;
     //for (uint64_t x = 0; x < tuple_count; x++) {
-        Array a_source;
-        initArray(&a_source, tuple_count);
-        //if (x % resolution == 0) {
-            uint64_t y;
-            for (y = 0; y < tuple_count; y++) {
-                insertion_count += 1;
-                insertArray(&a_source, fulldata[y].close);
-                if (y % ssize == 0) {
-                    double similarity = cosine_similarity(
-                        a_source.array, a_target.array, ssize);
-                    search_count += 1;
-                    if (similarity > thresh) {
-                        debugArray(&a_source, ssize);
-                        printf(" <<<--- [%lf] --->>> ", similarity);
-                        debugArray(&a_target, ssize);
-                        printf("\n\n");
-                    }
-                    freeArray(&a_source);
-                    initArray(&a_source, tuple_count);
-                }
+    struct array_t a;
+    init_array(&a, tuple_count);
+    //if (x % resolution == 0) {
+    uint64_t y;
+    for (y = 0; y < tuple_count; y++) {
+        insert_array(&a, fulldata[y].close);
+        if (y % ssize == 0) {
+            double similarity = cosine_similarity(a.array, b.array, ssize);
+            if (similarity > thresh) {
+                debug_array(&a, ssize);
+                printf(" <<<--- [%lf] --->>> ", similarity);
+                debug_array(&b, ssize);
+                printf("\n\n");
             }
-            //y += resolution;
-        //}
+            free_array(&a);
+            init_array(&a, tuple_count);
+        }
+    }
+    //y += resolution;
     //}
-    printf("--- search count: %lld ---\n", search_count);
-    printf("--- insertion count: %lld ---\n", insertion_count);
+    //}
     return 0;
 }
--- src/vision.h
+++ src/vision.h
@@ -6,11 +6,18 @@
 #include <stdint.h>
 #include <math.h>
 #include <signal.h>
+#include <unistd.h>
 #include <tarantool/tarantool.h>
 #include <tarantool/tnt_net.h>
 #include <tarantool/tnt_opt.h>
 #include <zmq.h>
 #include <msgpuck.h>
+
+struct array_t {
+    double *array;
+    size_t used;
+    size_t size;
+};
 
 struct crypto_t {
     uint64_t unix_val;
@@ -34,7 +41,14 @@ struct query_t {
 int tarantool_insert(struct crypto_t *cd);
 // select data from tarantool
 int tarantool_select(struct query_t *query, struct crypto_t *cd);
+// 0mq
+int zmq_listen();
 // tests
 int selector_test();
+// array
+void init_array(struct array_t *a, size_t initial_size);
+void insert_array(struct array_t *a, int element);
+void free_array(struct array_t *a);
+void debug_array(struct array_t *a, uint64_t length);
 
 #endif
