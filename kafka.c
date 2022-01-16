5cfda2699510ff8e8a7179b81ec9b57625a61ffd
alex_malder <vnmntn@gmail.com>
2022-01-16 21:48:41 +0300

    kafka-c

--- kafka.c
+++ kafka.c
@@ -25,22 +25,13 @@ static void dr_msg_cb(rd_kafka_t *rk, const rd_kafka_message_t *rkmessage,
                 rkmessage->len, rkmessage->partition);
 }
 
-int main(int argc, char **argv)
+int produce(char *buf)
 {
     rd_kafka_t *rk; /* Producer instance handle */
     rd_kafka_conf_t *conf; /* Temporary configuration object */
     char errstr[512]; /* librdkafka API error reporting buffer */
-    char buf[512]; /* Message value temporary buffer */
-    const char *brokers; /* Argument: broker list */
-    const char *topic; /* Argument: topic to produce to */
-
-    if (argc != 3) {
-        fprintf(stderr, "%% Usage: %s <broker> <topic>\n", argv[0]);
-        return 1;
-    }
-
-    brokers = argv[1];
-    topic = argv[2];
+    const char *brokers = "10.12.0.1:9092"; /* Argument: broker list */
+    const char *topic = "log"; /* Argument: topic to produce to */
 
     conf = rd_kafka_conf_new();
 
@@ -59,63 +50,12 @@ int main(int argc, char **argv)
 
     signal(SIGINT, stop);
 
-    fprintf(stderr, "%% Type some text and hit enter to produce message\n"
-                    "%% Or just hit enter to only serve delivery reports\n"
-                    "%% Press Ctrl-C or Ctrl-D to exit\n");
+    int err = rd_kafka_producev(rk, RD_KAFKA_V_TOPIC(topic),
+                                RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
+                                RD_KAFKA_V_VALUE(buf, strlen(buf)),
+                                RD_KAFKA_V_OPAQUE(NULL), RD_KAFKA_V_END);
 
-    while (run && fgets(buf, sizeof(buf), stdin)) {
-        size_t len = strlen(buf);
-        rd_kafka_resp_err_t err;
-
-        if (buf[len - 1] == '\n') /* Remove newline */
-            buf[--len] = '\0';
-
-        if (len == 0) {
-            /* Empty line: only serve delivery reports */
-            rd_kafka_poll(rk, 0 /*non-blocking */);
-            continue;
-        }
-
-    retry:
-        err = rd_kafka_producev(
-            /* Producer handle */
-            rk,
-            /* Topic name */
-            RD_KAFKA_V_TOPIC(topic),
-            /* Make a copy of the payload. */
-            RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
-            /* Message value and length */
-            RD_KAFKA_V_VALUE(buf, len),
-            /* Per-Message opaque, provided in
-                     * delivery report callback as
-                     * msg_opaque. */
-            RD_KAFKA_V_OPAQUE(NULL),
-            /* End sentinel */
-            RD_KAFKA_V_END);
-
-        if (err) {
-            /*
-                         * Failed to *enqueue* message for producing.
-                         */
-            fprintf(stderr, "%% Failed to produce to topic %s: %s\n", topic,
-                    rd_kafka_err2str(err));
-
-            if (err == RD_KAFKA_RESP_ERR__QUEUE_FULL) {
-                rd_kafka_poll(rk, 1000 /*block for max 1000ms*/);
-                goto retry;
-            }
-        } else {
-            fprintf(stderr,
-                    "%% Enqueued message (%zd bytes) "
-                    "for topic %s\n",
-                    len, topic);
-        }
-
-        rd_kafka_poll(rk, 0 /*non-blocking*/);
-    }
-
-    fprintf(stderr, "%% Flushing final messages..\n");
-    rd_kafka_flush(rk, 10 * 1000 /* wait for max 10 seconds */);
+    rd_kafka_flush(rk, 10 * 1000);
 
     if (rd_kafka_outq_len(rk) > 0)
         fprintf(stderr, "%% %d message(s) were not delivered\n",
@@ -123,4 +63,12 @@ int main(int argc, char **argv)
 
     rd_kafka_destroy(rk);
     return 0;
+}
+
+int main(int argc, char **argv)
+{
+    char buf[1024];
+    sprintf(buf, "{\"message\": \"text\", \"status\": %d}", 4);
+    int status = produce(buf);
+    printf("status: %d\n", status);
 }
