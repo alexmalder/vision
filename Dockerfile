0453005fb412d905b76a8430c258b48bceabaafd
alexmalder <alexmalder@mail.ru>
2022-07-24 01:36:53 +0300

    build-num-cpp

--- Dockerfile
+++ Dockerfile
@@ -1,45 +1,25 @@
-FROM alpine:3.14 as builder
-
+FROM alpine:3.16 as builder
 RUN apk update
-RUN apk add \
-    g++ \
-    git \
-    make \
-    cmake \
-    nlohmann-json \
-    msgpack-c-dev \
-    #zeromq-dev \
-    bash \
-    librdkafka-dev
-    #openssl-dev 
-#COPY ./cmake_install /cmake_install
-#RUN /cmake_install rtsisyk/msgpuck master
-#RUN /cmake_install tarantool/tarantool-c master
-
-RUN git clone https://github.com/yhirose/cpp-httplib /cpp-httplib
-RUN mkdir -p /usr/local/include
-RUN cp /cpp-httplib/httplib.h /usr/local/include/httplib.h
-RUN rm -rf /cpp-httplib
-
+RUN apk add g++ git make cmake nlohmann-json msgpack-c-dev bash librdkafka-dev
 ENV LD_LIBRARY_PATH=/usr/lib:/usr/local/lib
 WORKDIR /app
 COPY . .
 WORKDIR /app/build
 
+RUN git clone https://github.com/dpilger26/NumCpp
+WORKDIR /app/NumCpp/build
+RUN cmake .. -DBUILD_TESTS=ON -DBUILD_MULTIPLE_TEST=ON -DBUILD_EXAMPLE_GAUSS_NEWTON_NLLS=ON -DBUILD_EXAMPLE_README=ON
+RUN cmake --build . --config Release 
+RUN cmake --build . --config Release --target install
+
+WORKDIR /app
 RUN cmake ..
 RUN make
 RUN make install
 
 FROM alpine:3.14
 RUN apk update
-RUN apk add \
-    #zeromq \
-    librdkafka-dev \
-    msgpack-c \
-    nlohmann-json
-COPY ./data /data
-#openssl
-COPY --from=builder /usr/local/include/httplib.h /usr/local/include/httplib.h
+RUN apk add librdkafka-dev msgpack-c nlohmann-json
 COPY --from=builder /usr/local/lib /usr/local/lib
 COPY --from=builder /usr/lib /usr/lib
 COPY --from=builder /usr/local/bin/vision /usr/local/bin/vision
--- main.cpp
+++ main.cpp
@@ -6,45 +6,23 @@
 #include <sstream>
 #include <vector>
 #include <string>
-#include <httplib.h>
-
 #include "src/serdes.hpp"
 #include "src/crypto.hpp"
 
-
-void test_query() {
-    std::vector<crypto_t> crypto_data;
-    scan_data(crypto_data);
-    std::vector<std::string> messages;
-    vec_construct_origin(messages, crypto_data);
-    //push_data(messages, crypto_data);
-    query_t *query = new query_t();
-    query->searchio = 3;
-    query->start_date = 1641987866;
-    query->end_date = 1644666266;
-    query->user_id = 1;
-    query->resolution = 3;
-    query->thresh = 0.998;
-    vec_search(crypto_data, query);
-}
-
-int main()
+int main(void)
 {
-    httplib::Server svr;
-    std::vector<crypto_t> crypto_data;
-    scan_data(crypto_data);
-    svr.Post("/api/crypto", [&crypto_data](const httplib::Request &req, httplib::Response &res) {
-        nlohmann::json data = nlohmann::json::parse(req.body);
-        query_t *query = new query_t();
-        query->searchio = data["searchio"].get<int>();
-        query->start_date = data["start_date"].get<int>();
-        query->end_date = data["end_date"].get<int>();
-        query->user_id = data["user_id"].get<int>();
-        query->resolution = data["resolution"].get<int>();
-        query->thresh = data["thresh"].get<double>();
-        vec_search(crypto_data, query);
-        res.set_content("ok", "text/plain");
-    });
-    std::cout << "httplib server listening on port 9000" << std::endl;
-    svr.listen("0.0.0.0", 9000);
+	std::vector<crypto_t> crypto_data;
+	scan_data(crypto_data);
+	std::vector<std::string> messages;
+	vec_construct_origin(messages, crypto_data);
+	//push_data(messages, crypto_data);
+	query_t *query = new query_t();
+	query->searchio = 3;
+	query->start_date = 1641987866;
+	query->end_date = 1644666266;
+	query->user_id = 1;
+	query->resolution = 3;
+	query->thresh = 0.998;
+	vec_search(crypto_data, query);
+	return 0;
 }
